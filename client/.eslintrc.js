1053ab87b7b4e9d8ca901d6e7265b9b44a0f5e8c
Tim <16493467+Dev10000@users.noreply.github.com>
2022-09-29 23:23:35 +0300

    Client Install and Config Prettier and Eslint
    Install the following in client folder:
    Prettier
    - yarn add --dev --exact prettier
    - yarn add --dev eslint-config-prettier
    - yarn add --dev eslint-plugin-prettier
    
    Eslint config for React, Next.js, TypeScript using Airbnb style guide "npm init @eslint/config" "yarn next lint"
    - yarn add --dev eslint@latest
    - yarn add --dev eslint-config-airbnb@latest
    - yarn add --dev eslint-config-airbnb-typescript@latest
    - yarn add --dev @typescript-eslint/eslint-plugin@latest
    - yarn add --dev @typescript-eslint/parser@latest
    - yarn add --dev eslint-config-next
    // below peer dep for eslint-config-airbnb
    - yarn add --dev eslint-plugin-react@latest
    - yarn add --dev eslint-plugin-react-hooks@latest
    - yarn add --dev eslint-plugin-import@latest
    - yarn add --dev eslint-plugin-jsx-a11y@latest
    
    Refactor eslintrc.js and add some custom rules including Airbnb
    Refactor .eslintignore
    Fix Eslint errors manually and using 'npx eslint . --fix' after the new setup

---
+++ client/.eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'airbnb',
+    'airbnb-typescript',
+    'next/core-web-vitals',
+    'prettier',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: 'tsconfig.json',
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'no-param-reassign': ['error', { props: false }], // Fix np-param-reassign error in the reduxSlice
+    'react/jsx-props-no-spreading': 'warn',
+    'prettier/prettier': 'error',
+    'no-unused-vars': 'warn',
+    'no-console': 'off',
+    'func-names': 'off',
+    'no-process-exit': 'off',
+    'object-shorthand': 'off',
+    'class-methods-use-this': 'off',
+    'import/extensions': 'off',
+    'react/function-component-definition': 'off',
+    // index.tx arrow function Error: Function component is not a function declaration  react/function-component-definition
+    // 'react/function-component-definition': [
+    //   2,
+    //   {
+    //     namedComponents: 'arrow-function',
+    //     unnamedComponents: 'arrow-function',
+    //   },
+    // ],
+  },
+};
--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,7 +1,7 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import { store } from '../app/store';
 import { Provider } from 'react-redux';
+import { store } from '../app/store';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
